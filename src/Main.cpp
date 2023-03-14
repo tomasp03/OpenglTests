9cb655c23b7cba09aa74f9cb82c4feca1fb98609
tomasp03 <tomas.p03@seznam.cz>
2023-03-14 09:47:11 +0100

    added all

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
 
 #define WIN_W 1200
 #define WIN_H 1200
+
+std::string  LoadShader(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw(errno);
+}
+
+static void KeyPressed(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+float vertices[] =
+{
+	0.0, 0.5,
+	0.5, -0.5,
+	-0.5, -0.5
+};
 
 int main()
 {
@@ -14,9 +44,39 @@ int main()
 		return -1;
 	}
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "My window", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error: Failed to create window!" << std::endl;
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, KeyPressed);
+
+	if (!gladLoadGL())
+	{
+		std::cout << "Error: Failed to load glad!" << std::endl;
+		glfwTerminate();
+		return -3;
+	}
 
 	
 
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 }
